d6e1d70e3faadfaa0d2df85690cc7a8daf30fe2f
Pedro Luque Castaño <pedroluque481@gmail.com>
2025-06-10 16:02:35 -0300

    Ejercicio simple resuelto.

--- simple/simple.py
+++ simple/simple.py
@@ -11,7 +11,18 @@ def simple_list():
     return dicts
 
 def sort_list(dicts):
-    ordered_dicts = []
+    for i in range(1, len(dicts)):
+        actual = dicts[i]
+        j = i - 1
 
-    for i in range(len(dicts)):
-        pass
+        while j >= 0 and dicts[j]["age"] > actual["age"]:
+            dicts[j + 1] = dicts[j]
+            j -= 1
+        
+        dicts[j + 1] = actual
+    
+    return dicts
+
+diccionarios = simple_list()
+
+print(sort_list(diccionarios))
