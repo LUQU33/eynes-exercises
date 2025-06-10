1e35c446b2fbb58c01464d12e50459d033179dd3
Pedro Luque Castaño <pedroluque481@gmail.com>
2025-06-10 13:27:36 -0300

    Pruebas en el ejercicio de matriz.

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,12 @@
 def transpose(matrix):
-    pass
+
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    for i in range(len(matrix)):
+        for j in range (len(matrix[i])):
+            print(f"{matrix[i][j]}")
+
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+transpose(matriz)
