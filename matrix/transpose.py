fb8c380c7528a4e183ecfc9cab5798b0950166b1
Pedro Luque Castaño <pedroluque481@gmail.com>
2025-06-10 13:47:29 -0300

    Ejercicio matrix resuelto.

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -2,10 +2,23 @@ def transpose(matrix):
 
     rows = len(matrix)
     columns = len(matrix[0])
+    transpose_matrix = []
 
-    for i in range(len(matrix)):
-        for j in range (len(matrix[i])):
-            print(f"{matrix[i][j]}")
+    # Crear matriz transpuesta vacia
+    for i in range(columns):
+        new_row = []
+        for j in range(rows):
+            new_row.append(0)
+        transpose_matrix.append(new_row)
+
+    # Completar matriz transpuesta
+    for i in range(rows):
+        for j in range(columns):
+            transpose_matrix[j][i] = matrix[i][j]
+
+    # Mostrar matriz transpuesta
+    for i in range(len(transpose_matrix)):
+        print(f"{transpose_matrix[i]}")
 
 matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
