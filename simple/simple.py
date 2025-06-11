17a3b98d1673fb146911f8baf35817fff8164ff9
Pedro Luque Castaño <pedroluque481@gmail.com>
2025-06-11 11:32:45 -0300

    Correciones para cumplir requerimientos del ejercicio.

--- simple/simple.py
+++ simple/simple.py
@@ -1,15 +1,16 @@
 import random
 
-
+# Funcion que genera los 10 diccionarios
 def simple_list():
     dicts = []
 
     for i in range(10):
-        diccionario = {"id": i+1, "age": random.randint(1, 99)}
+        diccionario = {"id": i+1, "age": random.randint(1, 100)}
         dicts.append(diccionario)
     
     return dicts
 
+# Funcion que ordena los diccionarios por edad en orden ascendente
 def sort_list(dicts):
     for i in range(1, len(dicts)):
         actual = dicts[i]
@@ -26,5 +27,6 @@ def sort_list(dicts):
 diccionarios = simple_list()
 diccionarios_ord = sort_list(diccionarios)
 
+# For para muestra por pantalla de los diccionarios ordenados
 for i in range(len(diccionarios)):
     print(f"DICCIONARIO[{i}]: {diccionarios_ord[i]}")
